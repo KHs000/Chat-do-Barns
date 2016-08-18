7b3401b53a9f938983518bbc984358422fc8b37b
Aluno <Aluno@DESKTOP-PALV0JQ>
2016-08-18 10:47:18 -0300

    DOMAIN

--- Barns_Common/src/br/cefetmg/inf/ibarns/IBarns.java
+++ Barns_Common/src/br/cefetmg/inf/ibarns/IBarns.java
@@ -6,7 +6,7 @@ package br.cefetmg.inf.ibarns;
  * @author Felipe Rabelo
  */
 public interface IBarns {
-    public String pm (String sender, String receiver, String msg);
-    public String gm (String sender, Group receiver, String msg);
-    public String am (String sender, String msg);
+    public String sendPrivateMessage (User sender, User receiver, String msg);
+    public String sendToGroup (String sender, Group receiver, String msg);
+    public String sendToAll (String sender, String msg);
 }
