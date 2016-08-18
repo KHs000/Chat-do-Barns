45f50f17ccb1ac83ce9b2deae07d89f9dc57edf8
Felipe Rabelo <carbonemasseo@hotmail.com>
2016-08-18 09:59:45 -0300

    Projeto do servidor

---
+++ Barns_Service/src/br/cefetmg/inf/barns/Barns_Service.java
@@ -0,0 +1,14 @@
+
+package br.cefetmg.inf.barns;
+
+/**
+ *
+ * @author Felipe Rabelo
+ */
+public class Barns_Service {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Barns_Service/src/br/cefetmg/inf/barns/proxy/BarnsSkeleton.java
@@ -0,0 +1,17 @@
+
+package br.cefetmg.inf.barns.proxy;
+
+import java.net.Socket;
+
+/**
+ *
+ * @author Aluno
+ */
+public class BarnsSkeleton {
+    
+    private Socket socket;
+    
+    public BarnsSkeleton (Socket socket) {this.socket = socket;}
+    
+    
+}
