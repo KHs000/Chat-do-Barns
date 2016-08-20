78b1ccfe8a194cdc1a176c35257b76b713d064b2
98732 <rfr0067@hotmail.com>
2016-08-20 10:41:02 -0300

    Unicidade de usuários em grupos, e lista de receivers garantida; Projeto Barns_view adicionado a estrutura do projeor

---
+++ Barns_View/src/barns_view/constants/ReadErrorConstants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barns_view.constants;
+
+/**
+ *
+ * @author Conta Única */
+public class ReadErrorConstants {
+   public static final int RECEIVER_NOT_FOUND = 159;
+   public static final int SUCESSFUL_MESSAGE = 0;
+   public static final int SINTAX_ERROR = 147;
+}
